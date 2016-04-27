bec28b13b840a175222a1e7a45542c6338ce7bba
Dave <dsieh@providigm.com>
2016-04-27 08:45:17 -0600

    Generating simple worksheet from data.

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@
 
 const mongoose = require('mongoose');
 require('../lib/bootstrap');
+const workbook = require('../lib/workbook');
 
 mongoose.connect(config.database.url);
 
@@ -34,5 +35,25 @@ const groupByUser = function(startTime, endTime) {
 groupByUser(new Date(2015, 0, 1), new Date(2016, 3, 24)).
   then( (gStats) => { 
     console.log("Stats: %j", gStats); 
+
+    var data = [ [ "User", "Event Type", "Total" ] ];
+    for (var i=0; i<gStats.length; i++) {
+      data.push([ gStats[i].user, gStats[i].eventType, gStats[i].total ]);
+    }
+    var ws_name = "User Activity";
+
+    var wb = new workbook.Workbook(), ws = workbook.sheet_from_array_of_arrays(data);
+
+    /* add worksheet to workbook */
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+
+    /* write file */
+    workbook.writeFile(wb, 'test.xlsx');
+
     process.exit();
   });
+
+/* original data */
+// var data = [[1,2,3],[true, false, null, "sheetjs"],["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"], ["baz", null, "qux"]]
+
