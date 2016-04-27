89cf0a2e6336b3844a2e7a95ccea4308007b5724
Dave <dsieh@providigm.com>
2016-04-27 09:37:43 -0600

    Raw stats dump.

--- bin/index.js
+++ bin/index.js
@@ -6,11 +6,25 @@
 
 const mongoose = require('mongoose');
 require('../lib/bootstrap');
+const dateSVC = require('../lib/date-service');
 const workbook = require('../lib/workbook');
+const async = require('async');
 
 mongoose.connect(config.database.url);
 
 const BillingActivity = require('../lib/billing-activity-model');
+const User = require('../lib/user-model');
+
+const eventTypeMap = [
+  'Invalid Event Type', // 0
+  'Login', // 1
+  'Logout', // 2
+  'Book View', // 3
+  'Page Thumbnail View', // 4
+  'Page View', // 5
+  'Page PDF View', // 6
+  'Page Download' 
+];
 
 const groupByUser = function(startTime, endTime) {
   console.log("From: %j to %j", startTime, endTime);
@@ -32,27 +46,59 @@ const groupByUser = function(startTime, endTime) {
   });
 };
 
-groupByUser(new Date(2015, 0, 1), new Date(2016, 3, 24)).
-  then( (gStats) => { 
-    console.log("Stats: %j", gStats); 
+var dates = dateSVC.pastWeekRanges(new Date(2015, 9, 8, 0, 0, 0));
+var ranges = [];
+for (var i=0; i<7; i++) {
+  ranges.push({ from: dates[i], to: dates[i+1]});
+}
 
-    var data = [ [ "User", "Event Type", "Total" ] ];
-    for (var i=0; i<gStats.length; i++) {
-      data.push([ gStats[i].user, gStats[i].eventType, gStats[i].total ]);
-    }
-    var ws_name = "User Activity";
+var wb = new workbook.Workbook();
+var ws = null;
+var ws_name = "User Activity";
+var data = [];
 
-    var wb = new workbook.Workbook(), ws = workbook.sheet_from_array_of_arrays(data);
+async.eachSeries(ranges, (range, cb) => {
+  groupByUser(range.from, range.to).
+    then( (gStats) => { 
+      console.log("Stats: %j", gStats); 
+      console.log("From: %j to: %j", range.from, range.to);
 
-    /* add worksheet to workbook */
-    wb.SheetNames.push(ws_name);
-    wb.Sheets[ws_name] = ws;
+      data.push([ range.from, range.to ]); 
+      data.push([ "User", "Event Type", "Total" ] );
+      async.eachSeries(gStats, (gStat, cbStat) => {
+        User.findOne({_id: gStat.user}).
+          then( (user) => {
+            data.push([ user.name, eventTypeMap[gStat.eventType], gStat.total ]);
+            cbStat();
+          }).
+          catch( cbStat );
+      }, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        data.push([]);
 
-    /* write file */
-    workbook.writeFile(wb, 'test.xlsx');
+        cb();
+      });
+    })
 
+}, (err) => {
+  if (err) {
+    console.error(err);
     process.exit();
-  });
+  }
+
+  var ws = workbook.sheet_from_array_of_arrays(data);
+
+  /* add worksheet to workbook */
+  wb.SheetNames.push(ws_name);
+  wb.Sheets[ws_name] = ws;
+
+  /* write file */
+  workbook.writeFile(wb, 'test.xlsx');
+  process.exit();
+});
+
 
 /* original data */
 // var data = [[1,2,3],[true, false, null, "sheetjs"],["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"], ["baz", null, "qux"]]
