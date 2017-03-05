3ae0a3a47401b1886901276a37d9649e656ed283
Dave <dj0hnve@gmail.com>
2017-03-05 17:46:12 +0000

    Removed commented out junk.

--- bin/indexSemi.js
+++ bin/indexSemi.js
@@ -31,150 +31,104 @@ mongoose.connect(config.database.url);
 
 const userActivity = require('../lib/user-activity');
 const acctActivity = require('../lib/account-activity');
-//const weeklyActivity = require('../lib/weekly-activity');
-
-//var ranges = dateSVC.pastWeekRanges(program.today);
 
 var wb = new workbook.Workbook();
 var ws_userActivity = null;
 var ws_userActivityName = "User Activity";
 var ws_acctActivity = null;
 var ws_acctActivityName = "Account Activity";
-//var ws_weeklyActivity = null;
-//var ws_weeklyActivityName = "Weekly Activity";
 var userActivityData = [];
 var acctActivityData = [];
-// var weeklyActivityData = [];
-// const FmtDateRange = `${dateFormat(ranges[0].from, DFormat)} - ${dateFormat(ranges[6].from, DFormat)}`;
 const FmtDateRange = `${dateFormat(RangeStart, DFormat)} - ${dateFormat(RangeEnd, DFormat)}`;
 
 console.log("Date range: %j", FmtDateRange);
 
-// async.eachSeries(ranges, (range, cb) => {
+/**
+ * We want to use the same range for both user groupings and 
+ * account groupings
+ */
+userActivity.groupByUser(RangeStart, RangeEnd).
+  then( (gStats) => { 
 
-  /**
-   * We want to use the same range for both user groupings and 
-   * account groupings
-   */
-  // userActivity.groupByUser(range.from, range.to).
-  userActivity.groupByUser(RangeStart, RangeEnd).
-    then( (gStats) => { 
+    userActivityData.push([ dateFormat(RangeStart, DFormat) ]); 
+    userActivityData.push([ "User","Login","Logout","Book View","Page View", "Page PDF View","Page Download" ] );
+    return userActivity.processUserStats(gStats).
+      then( (userStats) => {
+        for (var i in userStats) {
+          userActivityData.push([i, 
+                    userStats[i].login, 
+                    userStats[i].logout, 
+                    userStats[i].bookView, 
+                    userStats[i].pageView, 
+                    userStats[i].pdfView, 
+                    userStats[i].pageDownload]);
+        }
+        userActivityData.push([]);
+        // cb();
+      });
+  }).
+  then( () => {
+    return acctActivity.groupByAccount(RangeStart, RangeEnd).
+      then( (gStats) => {
+        acctActivityData.push([ dateFormat(RangeStart, DFormat) ]); 
+        acctActivityData.push([ "Account","Login","Logout","Book View","Page View", "Page PDF View","Page Download" ] );
+        return acctActivity.processAccountStats(gStats).
+          then( (aStats) => {
+            for (var i in aStats) {
+              acctActivityData.push([i, 
+                        aStats[i].login, 
+                        aStats[i].logout, 
+                        aStats[i].bookView, 
+                        aStats[i].pageView, 
+                        aStats[i].pdfView, 
+                        aStats[i].pageDownload]);
+            }
+            acctActivityData.push([]);
+            // cb(); // Done with range...
+          });
+      });
+  }).
+  then( () => {
+    var ws_userActivity = workbook.sheet_from_array_of_arrays(userActivityData);
+    var ws_acctActivity = workbook.sheet_from_array_of_arrays(acctActivityData);
 
-      userActivityData.push([ dateFormat(RangeStart, DFormat) ]); 
-      userActivityData.push([ "User","Login","Logout","Book View","Page View", "Page PDF View","Page Download" ] );
-      return userActivity.processUserStats(gStats).
-        then( (userStats) => {
-          for (var i in userStats) {
-            userActivityData.push([i, 
-                      userStats[i].login, 
-                      userStats[i].logout, 
-                      userStats[i].bookView, 
-                      userStats[i].pageView, 
-                      userStats[i].pdfView, 
-                      userStats[i].pageDownload]);
-          }
-          userActivityData.push([]);
-          // cb();
-        });
-    }).
-    then( () => {
-      return acctActivity.groupByAccount(RangeStart, RangeEnd).
-        then( (gStats) => {
-          acctActivityData.push([ dateFormat(RangeStart, DFormat) ]); 
-          acctActivityData.push([ "Account","Login","Logout","Book View","Page View", "Page PDF View","Page Download" ] );
-          return acctActivity.processAccountStats(gStats).
-            then( (aStats) => {
-              for (var i in aStats) {
-                acctActivityData.push([i, 
-                          aStats[i].login, 
-                          aStats[i].logout, 
-                          aStats[i].bookView, 
-                          aStats[i].pageView, 
-                          aStats[i].pdfView, 
-                          aStats[i].pageDownload]);
-              }
-              acctActivityData.push([]);
-              // cb(); // Done with range...
-            });
-        });
-    }).
-    //catch( (err) => {
-      //console.error(err);
-    //});
+    /* add worksheets to workbook */
+    wb.SheetNames.push(ws_userActivityName);
+    wb.Sheets[ws_userActivityName] = ws_userActivity;
 
-//}, (err) => {
-  //if (err) {
-    //console.error(err);
-    //process.exit();
-  //}
+    wb.SheetNames.push(ws_acctActivityName);
+    wb.Sheets[ws_acctActivityName] = ws_acctActivity;
 
-  // Now, do the weekly stuff
-  //weeklyActivity.groupByUser(ranges[0].from, ranges[6].to).
-    //then( (wStats) => {
-      //return weeklyActivity.processWeeklyStats(wStats).
-        //then( (stats) => {
-          //weeklyActivityData.push(["User","Book","County","Book View","Page View","Page PDF View","Page Download" ]);
-          //for (var i in stats) {
-            //var fields = i.split(',');
-            //weeklyActivityData.push([
-              //fields[0],
-              //fields[1],
-              //fields[2],
-              //stats[i].bookView,
-              //stats[i].pageView,
-              //stats[i].pdfView,
-              //stats[i].pageDownload]);
-          //}
+    workbook.writeFile(wb, fileName);
 
-        //});
-    //}).
-    then( () => {
-      var ws_userActivity = workbook.sheet_from_array_of_arrays(userActivityData);
-      var ws_acctActivity = workbook.sheet_from_array_of_arrays(acctActivityData);
-      // var ws_weeklyActivity = workbook.sheet_from_array_of_arrays(weeklyActivityData);
+    if (program.recipients) {
+      var mailOptions = {
+        from: 'vault@bighornimaging.com',
+        to: program.recipients.split(','),
+        subject: "Activity report for " + FmtDateRange,
+        html: "<p>The activity report for the dates including " + FmtDateRange + " is attached.</p><p><strong>Do not reply to this email.</strong> It has been sent by the vault application server.</p>",
+        attachments: [ { path: fileName } ]
+      };
 
-      /* add worksheets to workbook */
-      wb.SheetNames.push(ws_userActivityName);
-      wb.Sheets[ws_userActivityName] = ws_userActivity;
-
-      wb.SheetNames.push(ws_acctActivityName);
-      wb.Sheets[ws_acctActivityName] = ws_acctActivity;
-
-      //wb.SheetNames.push(ws_weeklyActivityName);
-      //wb.Sheets[ws_weeklyActivityName] = ws_weeklyActivity;
-
-      workbook.writeFile(wb, fileName);
-
-      if (program.recipients) {
-        var mailOptions = {
-          from: 'vault@bighornimaging.com',
-          to: program.recipients.split(','),
-          subject: "Activity report for " + FmtDateRange,
-          html: "<p>The activity report for the dates including " + FmtDateRange + " is attached.</p><p><strong>Do not reply to this email.</strong> It has been sent by the vault application server.</p>",
-          attachments: [ { path: fileName } ]
-        };
-
-        email.deliver(mailOptions).
-          then( (info) => {
-            console.log("Email sent: %j", info);
-            fs.unlink(fileName, (err) => {
-              if (err) { console.error(err) }
-              process.exit();
-            });
-          }).
-          catch( (err) => {
-            console.log("Error sending email: %j", err);
+      email.deliver(mailOptions).
+        then( (info) => {
+          console.log("Email sent: %j", info);
+          fs.unlink(fileName, (err) => {
+            if (err) { console.error(err) }
             process.exit();
           });
-      } else {
-        process.exit();
-      }
-
-    }).
-    catch( (err) => {
-      console.error(err);
+        }).
+        catch( (err) => {
+          console.log("Error sending email: %j", err);
+          process.exit();
+        });
+    } else {
       process.exit();
-    });
+    }
 
-// });
+  }).
+  catch( (err) => {
+    console.error(err);
+    process.exit();
+  });
 
